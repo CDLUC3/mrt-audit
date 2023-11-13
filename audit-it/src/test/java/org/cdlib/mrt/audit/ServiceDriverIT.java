242dae7e5f25048ccaf24db7b2ede51ad425ce61
Terry Brady <terrence.brady@ucop.edu>
2023-11-13 14:05:46 -0800

    Add integ test

--- audit-it/src/test/java/org/cdlib/mrt/audit/ServiceDriverIT.java
+++ audit-it/src/test/java/org/cdlib/mrt/audit/ServiceDriverIT.java
@@ -27,6 +27,7 @@ import org.apache.http.entity.mime.MultipartEntityBuilder;
 import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.Test;
@@ -65,6 +66,27 @@ public class ServiceDriverIT {
                 }
                 return "";
         }
+
+        @Test
+        public void testJsonState() throws HttpResponseException, IOException, JSONException {
+                String url = String.format("http://localhost:%d/%s/jsonstate", port, cp);
+                testNodeStatus(getJsonContent(url, 200), "NodesState");
+        }
+
+        @Test
+        public void testJsonStatus() throws HttpResponseException, IOException, JSONException {
+                String url = String.format("http://localhost:%d/%s/jsonstatus", port, cp);
+                testNodeStatus(getJsonContent(url, 200), "NodesStatus");
+        }
+
+        public void testNodeStatus(JSONObject json, String key) throws HttpResponseException, IOException, JSONException {
+                JSONArray jarr = json.getJSONArray(key);
+                assertTrue(jarr.length() > 0);
+                for(int i=0; i < jarr.length(); i++){
+                        assertTrue(jarr.getJSONObject(i).getBoolean("running"));
+                }
+        }
+
 
         @Test
         public void simpleAuditTest() throws IOException, JSONException, SQLException, InterruptedException {
